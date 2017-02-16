c0e4a915651922478b22c92125471f3c0ee5aad9
laliluielo <stsrock08@aol.com>
2017-02-16 01:03:59 -0500

    Initial Files
    see Title

---
+++ AGDG Bowling/Assets/CameraBallTrail.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBallTrail: MonoBehaviour {
+
+    public GameObject ball;
+
+    //private Camera trailCam;
+    private float offset;
+
+	// Use this for initialization
+	void Start () {
+        //trailCam = GetComponent<Camera>();
+        offset = transform.position.z - ball.transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        float zValue = Mathf.Clamp(ball.transform.position.z + offset, 0, 18f);
+        transform.position = new Vector3(transform.position.x, transform.position.y, zValue);
+        transform.LookAt(ball.transform);
+
+    }
+
+
+}
---
+++ AGDG Bowling/Assets/PinMarker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinMarker : MonoBehaviour {
+
+    public GameObject pinPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+        foreach (Transform child in transform)
+        {
+            Debug.Log(child.gameObject + "at " + child.transform.position);
+            GameObject pin = Instantiate(pinPrefab, child);
+            pin.transform.position = pin.transform.parent.position;
+            pin.transform.rotation = Quaternion.Euler(Vector3.up);
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+}
